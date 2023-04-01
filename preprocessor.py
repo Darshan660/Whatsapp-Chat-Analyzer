9f880132ca83083f303121d6e0f138f5160edc85
Darshan660 <85669807+Darshan660@users.noreply.github.com>
2023-04-01 19:01:33 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,14 +1,14 @@
 import re
 import pandas as pd
+from dateutil import parser
 def preprocess(data):
     pattern = '\d{1,2}\/\d{2,4}\/\d{2,4},\s\d{1,2}:\d{1,2}\s\w{2}\s-\s'
 
     message = re.split(pattern, data)[1:]
 
     dates = re.findall(pattern, data)
-
     df = pd.DataFrame({'user_message': message, 'message_date': dates})
-    df['message_date'] = pd.to_datetime(df['message_date'],format="%d/%m/%y, %I:%M %p - ")
+    df['message_date'] = df['message_date'].str.rstrip(' -').apply(parser.parse)
     df.rename(columns={'message_date': 'date'}, inplace=True)
 
     users = []
