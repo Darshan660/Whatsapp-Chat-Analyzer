f8a74eafea3e0e984402af01ab881c937fe93794
Darshan660 <85669807+Darshan660@users.noreply.github.com>
2023-04-11 15:14:15 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -250,7 +250,7 @@ def daily_timeline(selected_user, df):
     for i in range(daily_timeline.shape[0]):
         month = str(daily_timeline['only_date'][i].month).zfill(2)  # Extract month and pad with leading zeros if necessary
         day = str(daily_timeline['only_date'][i].day).zfill(2)  # Extract day and pad with leading zeros if necessary
-        time.append(datetime.strftime(daily_timeline['only_date'][i], '%b-%d'))
+        time.append(datetime.strftime(daily_timeline['only_date'][i], '%b-%d-%Y'))
 
     daily_timeline['time'] = time
 
